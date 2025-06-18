9f372e9f4a20c0f29b4920de16d88ccac59ac391
Simone <simone.stenisperron@ellevio.se>
2025-06-18 14:40:34 +0200

    new logo

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: "#242424",
         icons: [
           {
-            src: "/icon-192x192.png",
+            src: "/logo-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icon-512x512.png",
+            src: "/logo-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
