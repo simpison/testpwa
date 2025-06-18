ef07dc535f2acf4d14061df66349860e841f160c
Simone <simone.stenisperron@ellevio.se>
2025-06-18 14:23:50 +0200

    add manifest etc

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Todo List PWA",
+        short_name: "TodoPWA",
+        description: "A simple todo list app as a Progressive Web App",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/testpwa/",
 });
